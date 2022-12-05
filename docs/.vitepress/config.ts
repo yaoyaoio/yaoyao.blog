294671d7a5e7ce206f75d119b36974ee60decc9c
yaoyao <i@yaoyao.io>
2022-12-06 00:09:22 +0800

    commit posts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,23 +1,31 @@
 import {defineConfigWithTheme} from "vitepress";
-import {DecThemeConfig} from "vitepress-theme-december/packages/theme/config";
+import {DecThemeConfig} from "../../packages/theme/src/config";
+import {getPosts} from "../../packages/theme/src/render";
 
-export default defineConfigWithTheme<DecThemeConfig>({
-  title: "YaoYao's Blog",
-  description: 'Just playing around.',
-  themeConfig: {
-    siteTitle: "YaoYao's Blog",
-    nav: [
-      {text: 'Home', link: '/'},
-      {text: 'Archives', link: '/pages/archives'},
-      {text: 'About', link: '/pages/about'},
-    ],
-    footer: {
-      message: 'Released under the <a href="https://github.com/vuejs/vitepress/blob/main/LICENSE">MIT License</a>.',
-      copyright: 'Copyright © 2015-present <a href="https://github.com/yaoyaoio">Yao Yao</a>'
-    },
-    socialLinks: [
-      {icon: 'github', link: 'https://github.com/yaoyaoio/yaoyao.blog'},
-    ],
-    pageSize: 10,
-  }
-})
+
+async function config() {
+  return defineConfigWithTheme<DecThemeConfig>({
+    title: "YaoYao's Blog",
+    description: 'Just playing around.',
+    themeConfig: {
+      siteTitle: "YaoYao's Blog",
+      nav: [
+        {text: 'Home', link: '/'},
+        {text: 'Archives', link: '/pages/archives'},
+        {text: 'About', link: '/pages/about'},
+      ],
+      footer: {
+        message: 'Released under the <a href="https://github.com/vuejs/vitepress/blob/main/LICENSE">MIT License</a>.',
+        copyright: 'Copyright © 2015-present <a href="https://github.com/yaoyaoio">Yao Yao</a>'
+      },
+      socialLinks: [
+        {icon: 'github', link: 'https://github.com/yaoyaoio/yaoyao.blog'},
+      ],
+      posts: await getPosts('docs'),
+      pageSize: 10,
+    }
+  })
+}
+
+
+export default config()
