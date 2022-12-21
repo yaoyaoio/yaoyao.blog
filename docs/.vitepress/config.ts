60bc4ceddb5209bf995078f93e20f8205314790b
yaoyao <i@yaoyao.io>
2022-12-21 23:23:33 +0800

    update

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -7,6 +7,9 @@ async function config() {
   return defineConfigWithTheme<DecThemeConfig>({
     title: "YaoYao's Blog",
     description: 'Just playing around.',
+    head: [
+      ['link', {rel: 'icon', href: '/favicon.ico'}]
+    ],
     themeConfig: {
       siteTitle: "YaoYao's Blog",
       nav: [
